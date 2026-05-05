727d73d0adc64012c6aec11bf76ff18bf7658aac
ginsengcandy <kimsparadise0202@gmail.com>
2026-05-05 17:45:58 +0900

    fix: 트랜잭션 커밋 이후 메시지 발행되도록 수정

--- src/main/java/com/example/allinmarket/domain/restocknotification/sender/RestockNotificationSender.java
+++ src/main/java/com/example/allinmarket/domain/restocknotification/sender/RestockNotificationSender.java
@@ -10,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
 @Slf4j
@@ -24,11 +26,15 @@ public class RestockNotificationSender {
         RestockNotification notification = notificationRepository.save(
                 RestockNotification.of(subscription.getUserId(), productId)
         );
-
-        NotificationEvent event = NotificationEvent.from(notification);
-
-        redisPublisher.publish(event);
-
         subscription.send();
+        NotificationEvent event = NotificationEvent.from(notification);
+        TransactionSynchronizationManager.registerSynchronization(
+                new TransactionSynchronization() {
+                    @Override
+                    public void afterCommit() {
+                        redisPublisher.publish(event);
+                    }
+                }
+        );
     }
 }
